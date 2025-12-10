5d7958ef8d36981963a9f3fbd106d60fb95e69b7
IsaidSegovia <issasegovia0710@gmail.com>
2025-12-10 14:12:32 -0600

    Visor3DMCI

--- src/App.jsx
+++ src/App.jsx
@@ -1953,7 +1953,7 @@ function App() {
                 }}
               >
                 <img
-                  src="/src/logo-maestria_0006_Capa-0.png"
+                  src="https://visor3dmci.netlify.app/src/logo-maestria_0006_Capa-0.png"
                   alt=""
                   style={{
                     width: 70,
